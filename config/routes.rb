8f5e5f5dd3ded7822ec51469b11e94b5e9179917
Andy Leverenz <andy@dribbble.com>
2019-06-15 18:27:20 -0500

    Custom routing

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,17 @@ Rails.application.routes.draw do
       mount Sidekiq::Web => '/sidekiq'
     end
 
+  devise_for :users,
+    path: '', # optional namespace or empty string for no space
+    path_names: {
+      sign_in: 'login',
+      sign_out: 'logout',
+      password: 'secret',
+      confirmation: 'verification',
+      registration: 'register',
+      sign_up: 'signup'
+    }
 
-  devise_for :users
   root to: 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
